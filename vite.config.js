fbf76c26d0fd6d0d372c56d26affbb0444ce2876
baltsezhakd <baltsezhakd@gmail.com>
2026-04-08 15:00:49 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/fit_tracker/',
+  base: 'https://baltsezhakdm.github.io/fit_tracker/',
   plugins: [react()],
 })
