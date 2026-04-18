1099a391a09a249176f4925109fa13c3bcf3b21c
Dmitiy <93870918+BaltsezhakDm@users.noreply.github.com>
2026-04-18 16:31:55 +0000

    fix

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://test.sttgeo.ru',
+  baseURL: 'https://test.sttgeo.ru:8443',
 });
 
 api.interceptors.request.use((config) => {
